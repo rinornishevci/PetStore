48a6b765ae0916d11d4f1eb795f77ee180feb9c7
rinor <rinornishevci@gmail.com>
2017-12-08 14:25:35 +0100

    code format changes

--- src/main/java/hello/Customer.java
+++ src/main/java/hello/Customer.java
@@ -10,12 +10,16 @@ import javax.persistence.Id;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
     private Long id;
+    
     private String firstName;
+    
     private String lastName;
 
-    protected Customer() {}
+    protected Customer() {
+    }
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
@@ -29,18 +33,15 @@ public class Customer {
                 id, firstName, lastName);
     }
 
-// end::sample[]
+    public Long getId() {
+        return id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 }
-
